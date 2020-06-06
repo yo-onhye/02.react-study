b88b69878e1ae2054462bfe356011186e935a527
yo-onhye <woony0807@gmail.com>
2020-06-07 00:50:01 +0900

    Homework01 에러메세지 노출 방식 변경

--- Homework01/hw1/src/App.js
+++ Homework01/hw1/src/App.js
@@ -43,10 +43,10 @@ class App extends Component {
 		return (
 			<form className='react-form'>
 				<input name='email' value={this.state.email} onChange={this.handleUsername} />
-				<p className={`valid_txt ${ this.state.email && !isEmailVaild && "active"}`}>@가 없음</p>
+				{!isEmailVaild && this.state.email !== "" ? <p className='valid_txt'>@가 없음</p> : null}
 				<input name='password' value={this.state.password} onChange={this.handlePassword} />
-				<p className={`valid_txt ${this.state.password && !isPasswordVaild && "active"}`}>길이가 6 이상이 아님</p>
-				<button type='submit' disabled={isEmailVaild && isPasswordVaild ? null : "disabled"}>
+				{!isPasswordVaild && this.state.password !== "" ? <p className='valid_txt'>길이가 6 이상이 아님</p> : null}
+				<button type='submit' disabled={isEmailVaild && isPasswordVaild ? null : "disabled"} onClick={() => alert("success!")}>
 					submit
 				</button>
 			</form>
